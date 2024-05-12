6c7a095970b38d76463f85d4c5923c5e7c829b4d
Divanka Samarasinghe <119291780+divankase@users.noreply.github.com>
2024-05-12 14:47:38 +0530

    add new field

--- app/src/main/java/com/example/grocerygo/Grocery.kt
+++ app/src/main/java/com/example/grocerygo/Grocery.kt
@@ -1,3 +1,10 @@
 package com.example.grocerygo
 
-data class Grocery(val id:Int,val title:String,val content:String)
+// Ensure that this property exists in your Grocery class
+class Grocery(
+    val id: Int,
+    val title: String,
+    val content: String,
+    var isCompleted: Boolean // Add this property
+)
+
